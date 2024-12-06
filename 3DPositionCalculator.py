2e0b20d063e9c0f46cffe8237aa77c4d698a30d8
Aaron Danton <aarondanton04@hotmail.com>
2024-12-06 17:04:15 +0000

    Working Serial Connection between arduino and computer

---
+++ 3DPositionCalculator.py
@@ -0,0 +1,101 @@
+import time
+import serial
+import cv2
+import os
+import threading
+
+
+"""
+on = True;
+while True:
+    if on:
+        ser.write(bytes("OFF", "UTF-8"))
+        # print(ser.readline());
+        # print(ser.readline());
+        print("Turn Off")
+    else:
+        ser.write(bytes("ON", "UTF-8"))
+        # print(ser.readline());
+        # print(ser.readline());
+        print("Turn On")
+    on = not on
+    time.sleep(1)
+"""
+
+files = ["Output", "Output/0", "Output/90", "Output/180", "Output/270"]
+
+for file in files:
+    if not os.path.exists(file):
+            os.makedirs(file)
+
+cam_port = -1
+cam = cv2.VideoCapture(cam_port)
+
+class TakeCameraLatestPictureThread(threading.Thread):
+    def __init__(self, camera):
+        self.camera = camera
+        self.frame = None
+        self.ret = False
+        super().__init__()
+        # Start thread
+        self.canRun = True
+        self.start()
+
+    def run(self):
+        while self.canRun:
+            self.ret, self.frame = self.camera.read()
+
+latest_picture = TakeCameraLatestPictureThread(cam)
+
+ser = serial.Serial("/dev/ttyUSB0")
+ser.baudrate=115200
+# ser.timeout=5
+
+currentRotation = 0
+currentImage = 0
+
+MSG_RESET = bytes("R", "UTF-8")
+MSG_NEXT = bytes("N", "UTF-8")
+
+ser.write(MSG_RESET)
+print(ser.read(1))
+while True:
+    count = 0
+    print("NEXT")
+    ser.write(MSG_NEXT)
+
+    msg = ser.read(1)
+    print(msg)
+    output = msg.decode("UTF-8")
+    print(f"OUTPUT: {output.strip()}")
+    try:
+        code = output[0]
+        print(f"CODE: {code}")
+    except Exception:
+        continue
+
+    if code == 'S':
+        # result, image = cam.read()
+        while not latest_picture.ret:
+            time.sleep(0.01)
+        time.sleep(0.2)
+        if latest_picture.ret:
+            cv2.imwrite(f"Output/{currentRotation}/image{currentImage}.png", latest_picture.frame)
+        else:
+            print("Failed to take picture")
+            exit()
+        print(f"Finished {currentRotation}/image{currentImage}.png")
+        currentImage += 1
+        time.sleep(0.5)
+    elif code == 'F':
+        ser.write(MSG_RESET)
+        ser.read(1)
+        print("Please rotate the tree")
+        input("PRESS ENTER WHEN DONE")
+        currentRotation += 90
+        currentImage = 0
+
+    if (currentRotation == 360):
+        break
+
+latest_picture.canRun = False
---
+++ LEDTreeArduinoTester/LEDTreeArduinoTester.ino
@@ -0,0 +1,68 @@
+#include <FastLED.h>
+
+#define LED_COUNT 10
+
+#define DATA_PIN 2
+
+CRGB leds[LED_COUNT];
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) { ; }
+  // Serial.write("Connected\n");
+  // Serial.setTimeout(1000);
+
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, LED_COUNT);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+int currentLED = -1;
+
+void clearSerial() {
+  while (Serial.available() > 0) Serial.read();
+}
+
+void sendMsg(char msg) {
+  delay(20);
+  Serial.print(msg);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char code = Serial.read();
+
+    switch (code) {
+      case 'N':
+        if (currentLED < LED_COUNT - 1) {
+          // Serial.println("Next");
+          // Turn on LED, Turn off previous
+          if (currentLED >= 0)
+            leds[currentLED] = CRGB::Black;
+          FastLED.show();
+          leds[currentLED + 1] = CRGB::Blue;
+          FastLED.show();
+          sendMsg('S');
+        } else {
+          // Serial.println("Must Reset");
+
+          leds[currentLED] = CRGB::Black;
+          sendMsg('F');
+        }
+        currentLED += 1;
+        break;
+      case 'R':
+        for (int i = 0; i < LED_COUNT; i++)
+          leds[i] = CRGB::Black;
+        currentLED = -1;
+        // Serial.println("Reset");
+
+        sendMsg('R');
+        break;
+    }
+    clearSerial();
+    // Serial.print("| CODE: ");
+    // Serial.println(code);
+  }
+}
